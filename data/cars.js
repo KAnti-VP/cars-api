69c7b9a6f8fd57289861090ff3cf8b61901d6df0
Kormányos Antal <kormanyos.antal@vasvari.hu>
2025-03-03 14:04:20 +0100

    basic server

---
+++ data/cars.js
@@ -0,0 +1,7 @@
+const cars = [
+    {brand: "Audi", model: "A4", year: 2022},
+    {brand: "BYD", model: "Sealion", year: 2024},
+    {brand: "Citroen", model: "C4", year: 2023},
+]
+
+export default cars
